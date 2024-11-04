bf41bb6bee124eccaa34035141666b290d78f913
Milzam Hibatullah <hmilzam@gmail.com>
2024-11-04 15:52:09 +0700

    create: example if we are not use depedency injection

--- lib/view/user.view.dart
+++ lib/view/user.view.dart
@@ -9,10 +9,25 @@ class UserView extends StatefulWidget {
 
 class _UserViewState extends State<UserView> {
   final TextEditingController usernameController = TextEditingController();
-
+  // final userService = locator<UserService>();
+  ///example user service without locator
+  ///first we should create an instance for user service
+  final userService = UserService();
+  /*
+   * summary
+   * the code above in this case we should creating an object of user service
+   * imagin if we have multiple widget class
+   * we should create an instance for each widget
+   * get it appears to handle to solve problems creating instance repeatedly
+   * when we register at once. the instance is create only once then can be use
+   * for all class in the project
+   * also if in user service need to add params in constructor, if we not use get_it
+   * then we need to add params in all instance that we create in every class
+   * imagine if user service is defined in 30 class, so we should change it in 30 class
+   * use get_it we just change in setup locator service
+   */
   @override
   Widget build(BuildContext context) {
-    final userService = locator<UserService>();
 
     return Scaffold(
       appBar: AppBar(title: Text("User Page")),
